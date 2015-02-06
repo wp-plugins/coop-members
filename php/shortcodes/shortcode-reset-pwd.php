38fbb38ab566f063f11f1c7eac740331d8037b6d
Bryan3598 <Bryan3598@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-06 01:05:55 +0000

    마이너 업데이트 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/coop-members/tags/1.0.3@1083554 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- php/shortcodes/shortcode-reset-pwd.php
+++ php/shortcodes/shortcode-reset-pwd.php
@@ -31,7 +31,7 @@ function wpc_reset_pwd($attr){
             $user = get_user_by('email', $username);
         } else {
             $error[] = '<p>' . __('이메일이나 아이디를 찾을 수 없습니다.') . '</p>';
-        }
+        } 
         if ( $user_exists ){
 
             $user_login = $user->user_login;
@@ -41,13 +41,13 @@ function wpc_reset_pwd($attr){
             // Now insert the new pass md5'd into the db
             $wpdb->query("UPDATE $wpdb->users SET user_activation_key = '$key' WHERE user_login = '$user_login'");
             //create email message
-            $message = __('누군가 목조주택시공자협동조합 웹사이트의 비밀번호 변경을 요청하였습니다.') . "\r\n\r\n";
+            $message = __('누군가 '.get_option('blogname').' 웹사이트의 비밀번호 변경을 요청하였습니다.') . "\r\n\r\n";
             $message .= get_option('siteurl') . "\r\n\r\n";
             $message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
             $message .= __('비밀번호를 변경하기 위해서는 하기 주소를 클릭하시기 바랍니다. 변경을 원치 않으시면 무시하셔도 좋습니다.') . "\r\n\r\n";
             $message .= get_option('siteurl') . "/wp-login.php?action=rp&key=$key\r\n";
             //send email meassage
-            $headers = "From: 목조주택시공자협동조합 <msh@wpcoop.kr>". "\r\n\\";
+            $headers = "From: ".get_option('blogname')." <".get_option('admin_email').">". "\r\n\\";
             if (!wp_mail($user_email, sprintf(__('[%s] Password Reset'), get_option('blogname')), $message, $headers) ){
               $error[] = '<p>' . __('이메일을 보낼 수 없습니다. 관리자에게 문의하시기 바랍니다.') . "<br />\n" . __('Possible reason: your host may have disabled the mail() function...') . '</p>';
           }
